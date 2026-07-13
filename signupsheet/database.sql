3aa579290fb44dbbc39b5603a9d7369245858bad
kaijonas25 <161161305+kaijonas25@users.noreply.github.com>
2026-07-13 11:42:59 -0700

    h

--- signupsheet/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE user_data;
-USE user_data;
-CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    age INT,
-    email VARCHAR(100),
-    phone VARCHAR(20)
-);
