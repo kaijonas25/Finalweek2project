3a478f643b602c4653cbce80103a3b94f05e7f60
Nico <nicholas.mj.choe@gmail.com>
2026-07-13 11:10:22 -0700

    incoming database code

---
+++ signupsheet/database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE user_data;
+USE user_data;
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    email VARCHAR(100),
+    phone VARCHAR(20)
+);
